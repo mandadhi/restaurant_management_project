c03872569d599d3ffcfe5150859369075fe32adc
Mandadhi Harsha Vardhan Reddy <harshavardhanreddy1715@gmail.com>
2025-08-07 10:20:57 +0530

    25006

--- home/views.py
+++ home/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.conf import settings
 
+def home(request):
+    restaraunt_name=settings.restaraunt_name
+    return render(request,'homepage.html',{'restaraunt_name':restaraunt_name})
 # Create your views here.
