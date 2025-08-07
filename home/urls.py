8202cc23b1ea1274acc8e803ebc7f1cd462742f1
Harsha <harshavardhanreddy1715@gmail.com>
2025-08-07 11:24:40 +0530

    Display restaraunt name

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import *
+from . import views
 
 urlpatterns = [
-    path('',views.home),
+    path('home',views.home),
     
 ]
--- home/views.py
+++ home/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.conf import settings
 
 def home(request):
-    restaraunt_name=settings.restaraunt_name
-    return render(request,'homepage.html',{'restaraunt_name':restaraunt_name})
+    restaraunt_name=settings.RESTARAUNT_NAME
+    return render(request,'restaraunt_home.html',{'restaraunt_name':restaraunt_name})
 # Create your views here.
