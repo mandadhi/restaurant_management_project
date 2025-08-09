4263581990c290673658fae8b74a09f07af1c135
Mandadhi Harsha Vardhan Reddy <harshavardhanreddy1715@gmail.com>
2025-08-10 00:12:48 +0530

    25004

--- home/views.py
+++ home/views.py
@@ -5,3 +5,5 @@ def home(request):
     restaraunt_name=settings.RESTARAUNT_NAME
     return render(request,'restaraunt_home.html',{'restaraunt_name':restaraunt_name})
 # Create your views here.
+def custom_404(request,exception):
+    return render(request,'page_404.html',status=404)
