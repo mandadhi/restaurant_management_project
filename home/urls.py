49081b84cf2126972a97bdf08aec22bdb71096f9
Mandadhi Harsha Vardhan Reddy <harshavardhanreddy1715@gmail.com>
2025-08-10 00:14:07 +0530

    25004

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from django.conf.urls import handler404
 from . import views
+from .views import custom_404
 
+handler404=custom_404
 urlpatterns = [
     path('home',views.home),
     
