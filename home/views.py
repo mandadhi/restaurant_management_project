affbf5406bfb44db2755530e182bcbffc7c5e69c
Mandadhi Harsha Vardhan Reddy <harshavardhanreddy1715@gmail.com>
2025-08-10 16:30:11 +0530

    25007

--- home/views.py
+++ home/views.py
@@ -7,3 +7,5 @@ def home(request):
 # Create your views here.
 def custom_404(request,exception):
     return render(request,'page_404.html',status=404)
+def about(request):
+    return render(request,'Aboutus.html')
